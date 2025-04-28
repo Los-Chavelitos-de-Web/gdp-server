1344618807f93483d666d6511cb1a345f3d69d74
Letalandroid <carlossoncra@gmail.com>
2025-04-28 10:30:13 -0500

    Habilitar CORS en la aplicación para permitir solicitudes desde cualquier origen

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   console.log('hola');
+
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
